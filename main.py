c7b437b71e0c1c65c11cc052ccf99dc5c2f57098
gaoissac2019 <48165303+gaoissac2019@users.noreply.github.com>
2023-05-22 00:43:09 +0200

    generating PDF file with first parameter.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import glob
 from fpdf import FPDF
+from pathlib import  Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     print(df)
-    #FPDF.add_page(orientation="L", format="A4")
-    #col1, col2, col3 in FPDF.cell():
+
+    pdf = FPDF(orientation="L", unit="mm", format="A4")
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice Nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
+
 
